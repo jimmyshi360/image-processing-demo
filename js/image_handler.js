bb6de7e30246765078def4649c52aebacb7ea950
Jimmy Shi <jimmyshi360@gmail.com>
2019-03-01 23:47:57 -0500

    setup basic client-side setup for viewing images

---
+++ js/image_handler.js
@@ -0,0 +1,35 @@
+/** Created by Jimmy Shi.
+A simply js file for applying image transformations imported from npm.
+**/
+
+let img;
+let imageLoader = document.getElementById('image-upload');
+    imageLoader.addEventListener('change', handleImage, false);
+let cvs = document.getElementById('image-display');
+let ctx = cvs.getContext('2d');
+
+//handles image uploading
+function handleImage(e){
+    let reader = new FileReader();
+	//image file has been uploaded
+    reader.onload = function(event){
+        img = new Image();
+		//update canvas image
+        img.onload = function(){
+            cvs.width = img.width;
+            cvs.height = img.height;
+            ctx.drawImage(img,0,0);
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);     
+}
+
+let imageTransformLibrary = require('micro-image-transformations');
+
+function makeGray() {
+	let newImage=grayscale(img);
+	 newImage.onload = function(){
+            ctx.drawImage(newImage,0,0);
+        }
+}
